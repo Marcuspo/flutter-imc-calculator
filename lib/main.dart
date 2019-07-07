8935131a9aa0c0f13f91fc396bdc4c412fd83686
Marcus <marcusporama@gmail.com>
2019-07-07 03:33:30 -0300

    Criando campos para inserção de dados

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,61 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Calculadora IMC"),
         centerTitle: true,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.black,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
             onPressed: () {},
+
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_pin, size: 120, color: Colors.black,),
+            TextField(keyboardType: TextInputType.number, 
+                      decoration: InputDecoration(
+                        labelText: "Peso em KG", 
+                        labelStyle: TextStyle(color: Colors.black)
+                      ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25.0,
+              ),
+            ),
+            TextField(keyboardType: TextInputType.number, 
+                      decoration: InputDecoration(
+                        labelText: "Altura em CM", 
+                        labelStyle: TextStyle(color: Colors.black)
+                      ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25.0,
+              ),
+            ),
+            Container(
+              height: 50,
+              child: 
+                RaisedButton(
+                onPressed: () {},
+                child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 20),),
+                color: Colors.black,
+              ),
+            ),
+            Text("Info", 
+              textAlign: TextAlign.center, 
+              style: TextStyle(
+                color: Colors.black, fontSize: 25
+                )
+              )
+          ],
+        ),
+      )
     );
   }
 }
