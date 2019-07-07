4cade0ff7f327ea182d803dfc9d6c051404e5d42
Marcus <marcusporama@gmail.com>
2019-07-07 03:40:28 -0300

    Finalizando laytou

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 void main () {
   runApp(MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
+    title: "Calculadora IMC",
     ));
 }
 
@@ -30,6 +32,7 @@ class _HomeState extends State<Home> {
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
           child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
@@ -56,13 +59,17 @@ class _HomeState extends State<Home> {
               fontSize: 25.0,
               ),
             ),
-            Container(
-              height: 50,
-              child: 
-                RaisedButton(
-                onPressed: () {},
-                child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 20),),
-                color: Colors.black,
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 10),
+              child:
+                Container(
+                height: 50,
+                child: 
+                  RaisedButton(
+                  onPressed: () {},
+                  child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 20),),
+                  color: Colors.black,
+                ),
               ),
             ),
             Text("Info", 
